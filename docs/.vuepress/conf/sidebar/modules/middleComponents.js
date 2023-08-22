1d88cc1b3f030580096b4e00142caf8251ab3528
wenjin <378752389@qq.com>
2023-08-22 16:42:27 +0800

    feat: nginx路径重写和重定向

--- docs/.vuepress/conf/sidebar/modules/middleComponents.js
+++ docs/.vuepress/conf/sidebar/modules/middleComponents.js
@@ -28,6 +28,9 @@ module.exports = [
             '/中间件/nginx/静态服务配置',
             '/中间件/nginx/动态代理配置',
             '/中间件/nginx/SSL证书配置',
+            '/中间件/nginx/防盗链',
+            '/中间件/nginx/路径重写',
+            '/中间件/nginx/线上处理',
 
         ]
     },
